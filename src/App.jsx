ca58bbe10f8fc7169ebecde7d66730c96a82927c
Stephen Bos <stephen.bos1029@gmail.com>
2025-12-10 21:47:57 -0700

    clear local storage button

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import Header from "./components/Header"
-import Menu from "./components/Menu"
-
+import Header from './components/Header'
+import Menu from './components/Menu'
 
 function App() {
-
-  return (
-    <>
-      <Header />
-      <hr className="h-4 bg-[#FC819E]"/>
-      <Menu />
-    </>
-  )
+   return (
+      <>
+         <Header />
+         <hr className="h-4 bg-[#FC819E]" />
+         <Menu />
+         <button
+            onClick={() => {
+               localStorage.clear()
+               window.location.reload()
+            }}
+         >
+            Reset App Storage
+         </button>
+      </>
+   )
 }
 
 export default App
